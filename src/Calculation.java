bd2468a22a3dee4994657cf8eea3ff327bec3ce3
Vladimir Yaltsev <yalt.vladimir@gmail.com>
2021-12-03 14:34:28 +0300

    Calculation/symbolChecking is added

--- src/Calculation.java
+++ src/Calculation.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Calculation {
+import java.util.NoSuchElementException;
+
+public class Calculation {
+
+    public String convertToPRN (String inText){
+return null;
+    }
+    public double calculator(String textPRN){
+        return 0;
+    }
+
+    private int symbolChecking(char symbol){
+
+        if(symbol == '*' || symbol == '/')return 3;
+        if(symbol == '+' || symbol == '-') return 2;
+        if(symbol == '(') return 1;
+        if(symbol == ')') return -1;
+
+        throw new NoSuchElementException("Please, type only symbols: 0-9, *,/,+,-,(,)");
+    }
+
 }
