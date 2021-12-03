6a88058ec1bfce48a065c38f0fc17dfb40d0fab6
Vladimir Yaltsev <yalt.vladimir@gmail.com>
2021-12-04 01:14:52 +0300

    Class Converter is added

--- src/Solution.java
+++ src/Solution.java
@@ -6,8 +6,9 @@ public class Solution {
     public static void main(String[] args) throws IOException {
       Reader reader = new Reader();
       //reader.toPrint(reader.toRead());
-      Calculation calculation = new Calculation();
-      reader.toPrint(calculation.convertToPRN(reader.toRead()));
+     // Calculation calculation = new Calculation();
+      Converter converter = new Converter();
+      reader.toPrint(converter.convertToPRN(reader.toRead()));
 
 
     }
